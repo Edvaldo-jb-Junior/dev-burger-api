672d0e8f0be07dba711b538c703bdd7ec2f35d22
Edvaldo-jb-Junior <juniorb2325@gmail.com>
2025-01-28 13:45:21 -0300

    upload-multer

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,21 @@
 import { Router } from "express"
 
-import UserController from "./app/controllers/UserController";
+import multer from "multer"
+
+import multerConfig from "./config/multer"
+
+import UserController from "./app/controllers/UserController"
 
 import SessionController from "./app/controllers/SessionController"
 
 import ProductController from "./app/controllers/ProductController"
 
-const routes = new Router()
+const routes = new Router();
+
+const upload = multer(multerConfig)
 
 routes.post("/users", UserController.store);
 routes.post("/session", SessionController.store);
-routes.post("/product", ProductController.store);
+routes.post("/products", upload.single("file"), ProductController.store);
 
 export default routes;
